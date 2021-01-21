6460a0f1fda9c3a7783dc5215e1a66e7f1e9662d
Lucas Meyble <lucasmeyble123@gmail.com>
2021-01-21 12:19:20 -0300

    aula3: entendendo o problema do cliente

--- js_orientado_objeto/index.js
+++ js_orientado_objeto/index.js
@@ -1,3 +1,9 @@
-const nome = 'lucas';
-const cpf = 70993119454;
-const 
+const cliente1Nome = 'lucas';
+const cliente1Cpf = 70993119454;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = 'alice';
+const cliente2Cpf = 70993121984;
+const cliente2Agencia = 1002;
+const cliente2Saldo = 100;
