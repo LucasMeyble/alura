c2ea6e658f29a578936330543d425640e162cc93
Lucas Meyble <lucasmeyble123@gmail.com>
2021-01-25 13:52:47 -0300

    referenciando cliente a um espaço de memoria, posso instaciar uma variavel com referencia a outro objeto

--- js_orientado_objeto/index.js
+++ js_orientado_objeto/index.js
@@ -7,8 +7,7 @@ Cliente1.nome = 'lucas';
 Cliente1.cpf = 70993119454;
 
 const Cliente2 = new Cliente();
-Cliente2.nome = 'alice';
-Cliente2.cpf = 70993121984;
+
 
 const contaCorrenteLucas = new ContaCorrente();
 contaCorrenteLucas.agencia = 1001;
@@ -16,10 +15,11 @@ contaCorrenteLucas.cliente = Cliente1;
 contaCorrenteLucas.depositar(500);
 
 const contaCorrenteAlice = new ContaCorrente();
-contaCorrenteAlice.cliente = Cliente2;
+contaCorrenteAlice.cliente = new Cliente();
+contaCorrenteAlice.cliente.nome = 'alice';
+contaCorrenteAlice.cliente.cpf = 70993121984;
 contaCorrenteAlice.agencia = 1002;
 
 contaCorrenteLucas.transferir(200, contaCorrenteAlice);
 
-console.log(contaCorrenteLucas);
 console.log(contaCorrenteAlice);
