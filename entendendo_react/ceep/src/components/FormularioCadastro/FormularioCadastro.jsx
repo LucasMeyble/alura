b6012cb6fd95d06c8cd6dedfe4ba7f468fe167d3
Lucas Meyble <lucasmeyble123@gmail.com>
2021-05-11 15:44:52 -0300

    lidando com eventos

--- entendendo_react/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ entendendo_react/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import './style.css'
 class FormularioCadastro extends Component{
+
+    constructor(){
+        super();
+        this.titulo='';
+
+    }
+
+    handleMudancaTitulo(evento){
+        this.titulo = evento.target.value;
+        console.log(this.titulo);
+    }
+    
     render(){
         return(
             <form className='form-cadastro'>
                 <input 
                     className='form-cadastro_input' 
                     type="text" 
-                    placeholder="Titulo" 
+                    placeholder="Titulo"
+                    onChange={this.handleMudancaTitulo}
                 />
                 <textArea 
                     rows={15} 
